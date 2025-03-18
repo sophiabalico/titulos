05a66faba65060f526d49afd53bc2cefbed3a383
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-03-18 09:56:22 -0300

    feat: create card components

--- src/app/titulos/page.jsx
+++ src/app/titulos/page.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styles from "./titulos.module.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Card from "../components/cards";
 
-const CorinthiansTrophiesPage = () => {
+export default function Titulo() {
   return (
     <div className={styles.container}>
       <Header />
@@ -11,20 +12,7 @@ const CorinthiansTrophiesPage = () => {
         <section className={styles.featured}>
           <h2>Conquistas Principais</h2>
           <div className={styles.featuredTrophies}>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Mundial de Clubes da FIFA 2012"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Mundial de Clubes da FIFA</h3>
-                <p className={styles.year}>2012</p>
-                <p className={styles.details}>Corinthians 1 x 0 Chelsea</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
+            <Card ano={2012} capa="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg" detalhes={"Corinthians 1 x 0 Chelsea"} textAlt={ "Troféu do Mundial Interclubes 2012"} titulo={"Mundial de Clubes da FIFA"} />
             <div className={styles.trophy}>
               <div className={styles.trophyCover}>
                 <img
@@ -220,4 +208,3 @@ const CorinthiansTrophiesPage = () => {
   );
 };
 
-export default CorinthiansTrophiesPage;
